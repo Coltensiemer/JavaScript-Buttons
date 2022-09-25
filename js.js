dc299d09d88f907e79cb94a020d901da20f74990
Coltensiemer <colten50@hotmail.com>
2022-09-24 21:25:07 -0500

    updated
    Started. Added functions! It works

---
+++ js.js
@@ -0,0 +1,9 @@
+const button = 
+document.getElementById("btn-red");
+button.addEventListener('click', e => { 
+const body =
+document.getElementById("body") 
+    body.style.backgroundColor = "red" 
+    console.log('is pushed')
+
+})
