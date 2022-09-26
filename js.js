b61745160dcb05cfd2be131c52f342035a764179
Coltensiemer <colten50@hotmail.com>
2022-09-25 21:39:46 -0400

    update

--- js.js
+++ js.js
@@ -1,9 +1,26 @@
-const button = 
-document.getElementById("btn-red");
-button.addEventListener('click', e => { 
-const body =
-document.getElementById("body") 
-    body.style.backgroundColor = "red" 
-    console.log('is pushed')
+// for red button
+const redButton = document.getElementById('btn-1');
+let index = 0; 
+const colors = ['red', 'white']; 
+redButton.addEventListener ('click', function(){
+    document.body.style.backgroundColor = colors[index];
+    index = index >= colors.length - 1 ? 0 : index + 1;}
+    );
 
-})
+// for yellow button 
+
+const yellowButton = document.getElementById('btn-yellow');
+
+const colorsTwo = ['yellow', 'white']; 
+
+yellowButton.addEventListener('click', function(){
+     document.body.style.backgroundColor = colorsTwo[index];
+     index = index >= colorsTwo.length - 1 ? 0 : index + 1;}
+    );
+
+
+//change the buttons blue 
+const blueButton = document.getElementById('btn-blue'); 
+    blueButton.addEventListener ('click', function(){ 
+        document.getElementsByName('button').style.color = "blue";
+    }); 
